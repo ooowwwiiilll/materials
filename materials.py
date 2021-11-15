ad1691b2a25e34c5a0d0197fdbf28f666d04c4a5
ooowwwiiilll <92080715+ooowwwiiilll@users.noreply.github.com>
2021-11-15 18:08:59 +0700

    Update materials.py

--- materials.py
+++ materials.py
@@ -1,8 +1,21 @@
-from odoo import fields, models, api
-from odoo.exceptions import ValidationError
+from datetime import datetime, time
+from dateutil.relativedelta import relativedelta
+from itertools import groupby
+from pytz import timezone, UTC
+from werkzeug.urls import url_encode
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_is_zero
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+
 
 class Materials(models.Model):
     _name = 'materials'
+    _description = 'Material List'
+
 
     name = fields.Char(string='Name', required=True)
     code = fields.Char(string='Code', required=True)    
@@ -11,7 +24,25 @@ class Materials(models.Model):
         ('jeans', 'Jeans') ,
         ('cotton', 'Cotton') ,
     ], string='Type', required=True)
-    price = fields.Float(string='Buy Price', required=True) #Price in IDR
+    price = fields.Float(string='Buy Price', required=True, default=100.000) #Price in IDR
+    supplier_name = fields.Many2one('suppliers', string='Supplier', required=True)
+    picture = fields.Image(string="Image")
+    # exported = fields.Many2one('material.order',string="")
+
+    # @api.model
+    # def create(self, vals_list):
+    #     res = super().create(vals_list)
+    #     vals_list = {
+    #         'name' : res.name
+    #     }
+    #     self.env['material.order'].create(vals_list)
+    #     return res
+
+    # def confirm_order(self):
+    #     vals = {
+    #         'name' : self.name
+    #     }
+    #     self.env['material.order'].create(vals)
 
     @api.constrains('price')
     def check_price(self):
@@ -21,15 +52,3 @@ class Materials(models.Model):
             else :
                 True
 
-    supplier_id = fields.Many2one('suppliers', string='Supplier', required=True)
-
-class Suppliers(models.Model):
-    _name = 'suppliers'
-
-    name = fields.Char(string='Name', required=True)
-    email = fields.Char(string='Email', required=True)
-
-class Suppliers(models.Model):
-    _inherit = 'suppliers'
-
-    supply_list = fields.One2many("materials", "supplier_id", string="Supply List")
